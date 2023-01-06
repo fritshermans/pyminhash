142a50b25641b62001420ff8b507faefa9e1831d
fritshermans <post@fritshermans.nl>
2023-01-06 14:07:23 +0100

    bumps version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Frits Hermans'
 author = 'Frits Hermans'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1'
+release = '0.1.5'
 
 # -- General configuration ---------------------------------------------------
 
--- pyminhash/__init__.py
+++ pyminhash/__init__.py
@@ -1,3 +1,3 @@
 from .pyminhash import MinHash
 
-__version__ = '0.1.4'
+__version__ = '0.1.5'
